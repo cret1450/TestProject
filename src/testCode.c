161b2ccedd7b66cc63ee236f3a9b9ad1cbc00ed4
Diane Crets <cret1450@pacificu.edu>
2021-09-03 21:10:06 -0700

    initial commit

---
+++ src/testCode.c
@@ -0,0 +1,27 @@
+/****************************************************************************
+ File name:	 testCode.c
+ Author:     Diane Crets
+ Date:       9/3/2021
+ Class:      CS300
+ Assignment: C Examples
+ Purpose:    Simple C Demo
+ ****************************************************************************/
+
+#include <stdio.h>
+
+/****************************************************************************
+ Function:			main
+
+ Description:	Display hello world
+
+ Parameters:		None
+
+ Returned:			0 for Success
+ ****************************************************************************/
+int main ()
+{
+	printf ("Hello World, I am Diane");
+	return 0;
+}
+
+
